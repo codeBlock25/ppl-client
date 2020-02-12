67ebd39361645e5c63ee8a3d7a3534a8a576c028
classic-code <50813314+codeBlock25@users.noreply.github.com>
2020-02-12 22:42:25 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -56,7 +56,7 @@ function App(){
             component={Cases}
             path="/cases"
           />
-          <Redirect from="/" to="/profile" exact={true} />
+          <Redirect from="/" to="/login" exact={true} />
         </Switch>
       </section>
     </React.Fragment>
