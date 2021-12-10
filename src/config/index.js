bee8da46efe2cc1073edc58e4c92a3ba35faa77f
classic-code <amitech125@gmail.com>
2021-12-10 11:38:59 +0100

    Update index.js

--- src/config/index.js
+++ src/config/index.js
@@ -1,4 +1,4 @@
 export const isDev = process.env["NODE_ENV"] === "development";
 export const _localApiUrl = "http://localhost:4000/api";
-export const _liveApiUrl = "http://ppl_server.herokuapp.com/api";
+export const _liveApiUrl = "https://ppl-server-api.herokuapp.com/api";
 export const apiUrl = isDev ? _localApiUrl : _liveApiUrl;
